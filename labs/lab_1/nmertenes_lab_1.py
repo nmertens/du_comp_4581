7ea0408d373d0e0d08f4278e314abd1896098aa0
Nick Mertens <nickolasmertens95@gmail.com>
2021-12-30 10:40:18 -0700

    created lab_1.py file

---
+++ labs/lab_1/nmertenes_lab_1.py
@@ -0,0 +1,15 @@
+# Python version of Matrix Multiplication
+
+def printMatrix(m):
+    for row in m:
+        print(row)
+
+def matrixMult(A, B):
+    pass
+    # Write code here
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
