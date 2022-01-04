f920070cb6ddcb085c027804c500e7fd5124e0a5
Nick Mertens <nickolasmertens95@gmail.com>
2022-01-04 14:58:03 -0700

    minor changes to variable names

--- labs/lab_1/nmertenes_lab_1.py
+++ labs/lab_1/nmertenes_lab_1.py
@@ -1,23 +1,29 @@
 # Python version of Matrix Multiplication
 
 def printMatrix(m):
+    # iterate over rows
     for row in m:
         print(row)
 
 def matrixMult(A, B):
-    result = []
+    # check if matrices are compatiable
     if len(A[0]) == len(B):
+        # initialize results matrix
         result = []
-        for m in range(len(A)): 
+        # iterate over rows in A
+        for i in range(len(A)): 
             rows = []
-            for i in range(len(B[0])): 
+            # iterate over columns in B
+            for j in range(len(B[0])): 
                 columns = 0
-                for j in range(len(B)): 
-                    columns += A[m][j] * B[j][i]
+                # iterate over values in each column of B
+                for k in range(len(B)): 
+                    columns += A[i][k] * B[k][j]
                 rows.append(columns) 
             result.append(rows)
         return result
     else:
+        # raise error if inner dimensions do not match
         raise ValueError(f'Number of columns in first matrix ({len(A[0])}) do not equal number of rows in second matrix ({len(B)})')
 
 def main(A, B):
