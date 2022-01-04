7c65ae9b97582d6e6e0823f9ff163c7ab434736f
Nick Mertens <nickolasmertens95@gmail.com>
2022-01-04 14:26:19 -0700

    created matrix multiply function

--- labs/lab_1/nmertenes_lab_1.py
+++ labs/lab_1/nmertenes_lab_1.py
@@ -5,11 +5,23 @@ def printMatrix(m):
         print(row)
 
 def matrixMult(A, B):
-    pass
-    # Write code here
+    result = []
+    if len(A[0]) == len(B):
+        result = []
+        for m in range(len(A)): 
+            rows = []
+            for i in range(len(B[0])): 
+                columns = 0
+                for j in range(len(B)): 
+                    columns += A[m][j] * B[j][i]
+                rows.append(columns) 
+            result.append(rows)
+        return result
+    else:
+        raise ValueError(f'Number of columns in first matrix ({len(A[0])}) do not equal number of rows in second matrix ({len(B)})')
 
-def main():
-    pass
+def main(A, B):
+    print(matrixMult(A,B))
 
 if __name__ == '__main__':
     main()
