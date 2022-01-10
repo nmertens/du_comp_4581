2de7740467be808acf99bccde9eda06dad2d7e7f
Nick Mertens <nickolasmertens95@gmail.com>
2022-01-10 14:14:16 -0700

    final code implemented; testing successful

--- labs/lab_2/nmertens_lab_2.py
+++ labs/lab_2/nmertens_lab_2.py
@@ -1,4 +1,5 @@
 from time import time
+import random
 
 def merge(A, B):
     out = []
@@ -16,6 +17,7 @@ def merge(A, B):
     while j < len(B):
         out.append(B[j])
         j += 1
+    return out
 
 def mergeSort(L):
     if len(L) < 2:
@@ -26,14 +28,62 @@ def mergeSort(L):
         Right = mergeSort(L[mid:])
         return merge(Left, Right)
 
-def insertionSort(A):
-    for i in range(len(A)):
-        key = A[i]
+def insertionSort(L):
+    for i in range(len(L)):
+        key = L[i]
         j = i-1
-        while j >= 0 and A[j] > key:
-            A[j+1] = A[j]
+        while j >= 0 and L[j] > key:
+            L[j+1] = L[j]
             j = j-1
-        A[j+1] = key
+        L[j+1] = key
+    return L
+
+def bubbleSort(L):
+    # loop through list
+    for i in range(len(L)):
+        # loop to compare list elements
+        for j in range(0, len(L) - i - 1):
+            # compare two list elements
+            if L[j] > L[j+1]:
+                temp = L[j]
+                L[j] = L[j+1]
+                L[j+1] = temp
+    return L
 
 def main():
-    pass
+
+    # generate list of numbers to operate on
+    L = [i for i in range(100, 5100, 100)]
+
+    # print headers
+    print(f'N\tMerge\tInsert\tBubble')
+    
+    # loop through list L
+    for j in L:
+        # generate and shuffle list
+        numList = [i for i in range(j)]
+        random.shuffle(numList)
+        
+        # merge method
+        mergeT1 = time()
+        mergeData = mergeSort(numList)
+        mergeT2 = time()
+        mergeTime = round((mergeT2 - mergeT1)*1000, 1)
+
+        # insertion method
+        insertionT1 = time()
+        insertionData = insertionSort(numList)
+        insertionT2 = time()
+        insertionTime = round((insertionT2 - insertionT1)*1000, 1)
+
+        # bubble method
+        bubbleT1 = time()
+        bubbleData = bubbleSort(numList)
+        bubbleT2 = time()
+        bubbleTime = round((bubbleT2 - bubbleT1)*1000, 1)
+
+        # print results
+        print(f'{j}\t{mergeTime}\t{insertionTime}\t{bubbleTime}')
+
+if __name__ == '__main__':
+    main()
