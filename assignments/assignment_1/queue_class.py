85ef7de109395d1fb8d5eaef5cab01fe05774fa1
Nick Mertens <nickolasmertens95@gmail.com>
2022-01-19 08:43:18 -0700

    added doc strings

--- assignments/assignment_1/queue_class.py
+++ assignments/assignment_1/queue_class.py
@@ -8,29 +8,34 @@ class Queue(object):
         return myString
 
     def enqueue(self, item):
-        '''This function adds an item to the rear end of the queue '''
+        ''' Add item to the end of the queue 
+        '''
         if(self.isFull() != True):
             self.queue.insert(0, item)
         else:
             print('Queue is Full!')
 
     def dequeue(self):
-        ''' This function removes an item from the front end of the queue '''
+        ''' Remove an item from the start of the queue 
+        '''
         if(self.isEmpty() != True):
             return self.queue.pop()
         else:
             print('Queue is Empty!')
 
     def isEmpty(self):
-        ''' This function checks if the queue is empty '''
+        ''' Check if the queue is empty 
+        '''
         return self.queue == []
 
     def isFull(self):
-        ''' This function checks if the queue is full '''
+        ''' Check if the queue is full 
+        '''
         return len(self.queue) == self.size
 
-    def peek(self):
-        ''' This function helps to see the first element at the fron end of the queue '''
+    def check(self):
+        ''' See the first element at the start of the queue 
+        '''
         if(self.isEmpty() != True):
             return self.queue[-1]
         else:
