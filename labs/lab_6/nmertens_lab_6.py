50ed111637d0f4dcd9ce3757c11fb0951a6b284f
Nick Mertens <nickolasmertens95@gmail.com>
2022-02-08 08:43:52 -0700

    minor code changes, fixed member function

--- labs/lab_6/nmertens_lab_6.py
+++ labs/lab_6/nmertens_lab_6.py
@@ -3,66 +3,66 @@
 
 import numpy as np
 
-class MyHashtable() :
+class MyHashtable():
     
-    def __init__(self,size) :
+    def __init__(self,size):
         self.size = size
-        self.hashtable = np.array([None]*self.size)
+        self.table = np.array([None]*self.size)
     
     def __str__(self):
-        return str(self.hashtable)
+        return str(self.table)
         
-    def hash(self,key) :
+    def hash(self,element):
         
         # Hash function h(x) = x%10
-        index = ord(key[0])%self.size
+        index = ord(element[0])%self.size
         
-        if self.hashtable[index] == None :
+        if self.table[index] == None:
             return index
-        else :
+        else:
             
             # Implementing linear probing
-            while self.hashtable[index] != None :
+            while self.table[index] != None:
                 index = (index+1)%self.size
                 
             return index
         
-    def insert(self,key) :
+    def insert(self,element):
         
-        index = self.hash(key)
-        self.hashtable[index] = key
+        index = self.hash(element)
+        self.table[index] = element
     
-    def delete(self, key):
-        index = ord(key[0])%self.size
+    def delete(self, element):
+        index = ord(element[0])%self.size
         
-        if self.hashtable[index] != key :
-            while self.hashtable[index] != key and self.hashtable[index] != None :
+        if self.table[index] != element:
+            while self.table[index] != element and self.table[index] != None:
                 index = (index+1)%self.size
                 
-        if self.hashtable[index] == key :
-            self.hashtable[index] = 'Deleted'
-        else :
-            next     
+        if self.table[index] == element:
+            self.table[index] = 'Deleted'  
         
-    def search(self,key) :
+    def member(self,element):
         
-        index = ord(key[0])%self.size
+        index = ord(element[0])%self.size
         
-        if self.hashtable[index] != key :
-            while self.hashtable[index] != key and self.hashtable[index] != None :
+        if self.table[index] != element:
+            while self.table[index] != element and self.table[index] != None:
                 index = (index+1)%10
                 
-        if self.hashtable[index] == key :
-            return index
-        else :
-            return None
+        if self.table[index] == element:
+            # return index
+            return True
+        else:
+            # return None
+            return False
 
 s = MyHashtable(10)
 s.insert("amy") #97
 s.insert("chase") #99
 s.insert("chris") #99
-print(s.search("amy"))
-print(s.search("chris"))
-print(s.search("alyssa"))
+print(s.member("amy"))
+print(s.member("chris"))
+print(s.member("alyssa"))
 s.delete("chase")
-print(s.search("chris"))
+print(s.member("chris"))
