1da1af5f55e96377cdc64a21d4892b5249c604d7
Nick Mertens <nickolasmertens95@gmail.com>
2022-03-01 10:15:00 -0700

    final code changes

--- labs/lab_9/nmertens_lab_9.py
+++ labs/lab_9/nmertens_lab_9.py
@@ -1,47 +1,57 @@
-# Dijkstra's shortest path greedy algorithm
- 
-# Find the min priority vertex from the list of given vertices 
-# # Each vertex in the form of a list with priority as the first 
-# # element returns the min vertex and removes it from the list 
-def extractMin(verts):
-    minIndex = 0
-    for v in range(1,len(verts)):
-        if verts[v][1] < verts[minIndex][1]: 
-            minIndex = v
-    return verts.pop(minIndex)
-# Dijkstra's shortest path algorithm 
+# Prim's MST Alogrithm
+# Last updated: 3/1/2022
 
-def shortest(g):
+def extractMin(key, processed):
 
-    # Create a list of vertices and their current shortest distances 
-    # from vertex 0
-    # [vertNum, dist]
-    nVerts = len(g)
-    vertsToProcess = [[i, float("inf")] for i in range(nVerts)]
+    # initialize min value to be infinity
+    min = float("inf")
 
-    # Start at vertex 0 - it has a current shortest distance of 0
-    vertsToProcess[0][1] = 0
+    for v in range(len(processed)):
+        # only look at nodes that aren't processed
+        if key[v] < min and processed[v] == False:
+            min = key[v]
+            min_index = v
 
-    # Start with an empty list of processed edges 
-    vertsProcessed = []
-    while len(vertsToProcess) > 0:
-        u = extractMin(vertsToProcess) 
-        vertsProcessed.append(u)
-        #print("to process:",vertsToProcess) 
-        #print(" processed:",vertsProcessed)
-    # Examine all potential verts remaining 
-    for v in vertsToProcess:
-        # Only care about the ones that are adjacent to u 
-        if g[u[0]][v[0]] > 0:
-            # Update the distances if necessary 
-            if u[1] + g[u[0]][v[0]] < v[1]: 
-                v[1] = u[1] + g[u[0]][v[0]]
+    return min_index
+
+def mst(g):
+
+    nVerts = len(g) # number of vertices
+
+    key = [float("inf") for i in range(nVerts)] # array to store minimum weight
+    parent = [None for i in range(nVerts)]  # array to store parent nodes
+    processed = [False for i in range(nVerts)] # array to indicate what's been processed
+
+    # Set the root node and parent
+    key[0] = 0
+    parent[0] = -1
+
+    for i in range(nVerts):
+
+        # find the vertex with minimum distance vertex
+        u = extractMin(key, processed)
+
+        # once processed update status
+        processed[u] = True
+
+        for v in range(nVerts):
+            # only look at nodes that are adjacent and have not been processed
+            if g[u][v] > 0 and processed[v] == False:
+                # update key and parent if current weight is less than the value
+                if g[u][v] < key[v]:
+                    key[v] = g[u][v]
+                    parent[v] = u
+
+    # put everything together and return the output
+    output = []
+    for j in range(nVerts): 
+        output.append([j, parent[j]])
     
-    print(vertsProcessed)
+    return output
 
 # Adjacency matrix representation of a graph
 # This particular graph is the one from the videos 
-# # The vertices didn't have labels in the videos
+# The vertices didn't have labels in the videos
 # so I'm using the following vertex labels: 
 #   2
 #  / \
@@ -50,6 +60,7 @@ def shortest(g):
 # 4 | 0--6
 #  \|/
 #   5
+
 graph = [[0, 7, 0, 0, 0, 10, 15, 0],
          [7, 0, 12, 5, 0, 0, 0, 9], 
          [0, 12, 0, 6, 0, 0, 0, 0], 
@@ -58,4 +69,5 @@ graph = [[0, 7, 0, 0, 0, 10, 15, 0],
          [10, 0, 0, 8, 3, 0, 0, 0], 
          [15, 0, 0, 0, 0, 0, 0, 0], 
          [0, 9, 0, 0, 0, 0, 0, 0]]
-shortest(graph)
+
+print(mst(graph))
